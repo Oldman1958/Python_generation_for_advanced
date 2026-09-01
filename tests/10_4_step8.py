bb9f07c2ce297d2fc81bc4f6ce8eff795fcf2fa9
Oldman1958 <boris_i@bk.ru>
2026-09-01 17:23:05 +0500

    Самое редкое слово

--- tests/10_4_step8.py
+++ tests/10_4_step8.py
@@ -40,13 +40,3 @@ min_freq = min(counts.values())
 result = min(filter(lambda w: counts[w] == min_freq, counts.keys()))
 
 print(result)
-
-""" 
-London is the capital of Great Britain. 
-More than six million people live in London. 
-London lies on both banks of the river Thames. 
-It is the largest city in Europe and one of the largest cities in the world. 
-London is not only the capital of the country, 
-it is also a very big port, one of the greatest commercial centres in the world, 
-a university city, and the seat of the government of Great Britain!
-"""
